0eb57592742e7ad45defdc7259d56a22c26f6cd5
Laymax4 <max_vazquez-mats@hotmail.com>
2023-12-04 20:22:00 -0600

    Interfaz Registro de Asistencia
    Esta es la primera versión funcional de el Registro de Asistencia, faltando detalles de Inicio e imágenes.

--- RegistroDeAsistencia/Registro.Designer.cs
+++ RegistroDeAsistencia/Registro.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace RegistroDeAsistencia
 {
-    partial class Form1
+    partial class Registro
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,358 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            vScrollBar1 = new VScrollBar();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            comboBox2 = new ComboBox();
+            label7 = new Label();
+            comboBox3 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            Validación = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Apellido = new DataGridViewTextBoxColumn();
+            Boleta = new DataGridViewTextBoxColumn();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            label9 = new Label();
+            vScrollBar2 = new VScrollBar();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(691, 406);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 35);
+            button1.TabIndex = 0;
+            button1.Text = "Finalizar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 413);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(567, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // vScrollBar1
+            // 
+            vScrollBar1.Location = new Point(0, 0);
+            vScrollBar1.Name = "vScrollBar1";
+            vScrollBar1.Size = new Size(17, 20);
+            vScrollBar1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(175, 128);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(368, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(175, 99);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(368, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 426);
+            label1.Name = "label1";
+            label1.Size = new Size(10, 15);
+            label1.TabIndex = 8;
+            label1.Text = " ";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "6CM11", "6CM12", "6CM13", "6CM14", "6CM15", "6CM16", "6CV11", "6CV12", "6CV13", "6CV14", "6CV15", "6CV16" });
+            comboBox1.Location = new Point(175, 70);
+            comboBox1.Name = "comboBox1";
+            comboBox1.RightToLeft = RightToLeft.No;
+            comboBox1.Size = new Size(68, 23);
+            comboBox1.TabIndex = 9;
+            comboBox1.Text = "6CM11";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 416);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Observaciones:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(119, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Materia:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(120, 72);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Profesor:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(119, 102);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Grupo:    ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(249, 73);
+            label6.Name = "label6";
+            label6.Size = new Size(58, 15);
+            label6.TabIndex = 15;
+            label6.Text = "Semestre:";
+            label6.Click += label6_Click;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "2020-1", "2020-2", "2021-1", "2021-2", "2022-1", "2022-2", "2023-1", "2023-2" });
+            comboBox2.Location = new Point(313, 70);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(58, 23);
+            comboBox2.TabIndex = 14;
+            comboBox2.Text = "2023-1";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(381, 72);
+            label7.Name = "label7";
+            label7.Size = new Size(41, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Fecha:";
+            // 
+            // comboBox3
+            // 
+            comboBox3.AccessibleDescription = "DD/MM/YY";
+            comboBox3.AccessibleName = "DD/MM/YY";
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "DD/MM/YY", "DD/MM/YY" });
+            comboBox3.Location = new Point(428, 69);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(88, 23);
+            comboBox3.TabIndex = 16;
+            comboBox3.Text = "DD/MM/YY";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.Sunken;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Sunken;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Validación, Column2, Column3, Column4, Apellido, Boleta });
+            dataGridView1.GridColor = Color.BlueViolet;
+            dataGridView1.Location = new Point(25, 163);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RightToLeft = RightToLeft.Yes;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(736, 237);
+            dataGridView1.StandardTab = true;
+            dataGridView1.TabIndex = 20;
+            // 
+            // Validación
+            // 
+            Validación.Frozen = true;
+            Validación.HeaderText = "Validación";
+            Validación.MaxInputLength = 330000;
+            Validación.Name = "Validación";
+            Validación.Width = 115;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Escuela";
+            Column2.Name = "Column2";
+            Column2.Width = 115;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Nombre(s)";
+            Column3.Name = "Column3";
+            Column3.Width = 115;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Apellido M";
+            Column4.Name = "Column4";
+            Column4.Width = 115;
+            // 
+            // Apellido
+            // 
+            Apellido.HeaderText = "Apellido P";
+            Apellido.Name = "Apellido";
+            Apellido.Width = 115;
+            // 
+            // Boleta
+            // 
+            Boleta.HeaderText = "Boleta";
+            Boleta.Name = "Boleta";
+            Boleta.Width = 115;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Location = new Point(549, 66);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(91, 85);
+            groupBox1.TabIndex = 21;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "        Folio";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(24, 62);
+            label8.Name = "label8";
+            label8.Size = new Size(43, 15);
+            label8.TabIndex = 22;
+            label8.Text = "000001";
+            label8.Click += label8_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(24, 66);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 77);
+            button2.TabIndex = 22;
+            button2.Text = "Asistencia QR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(646, 69);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 82);
+            button3.TabIndex = 23;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(691, 128);
+            label9.Name = "label9";
+            label9.Size = new Size(41, 15);
+            label9.TabIndex = 23;
+            label9.Text = "Ayuda";
+            // 
+            // vScrollBar2
+            // 
+            vScrollBar2.Location = new Point(764, 163);
+            vScrollBar2.Name = "vScrollBar2";
+            vScrollBar2.Size = new Size(24, 237);
+            vScrollBar2.TabIndex = 24;
+            // 
+            // Registro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(vScrollBar2);
+            Controls.Add(label9);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label7);
+            Controls.Add(comboBox3);
+            Controls.Add(label6);
+            Controls.Add(comboBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(vScrollBar1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Registro";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private VScrollBar vScrollBar1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ContextMenuStrip contextMenuStrip1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ComboBox comboBox2;
+        private Label label7;
+        private ComboBox comboBox3;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private Label label8;
+        private Button button2;
+        private Button button3;
+        private Label label9;
+        private DataGridViewTextBoxColumn Validación;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Apellido;
+        private DataGridViewTextBoxColumn Boleta;
+        private VScrollBar vScrollBar2;
     }
 }
--- RegistroDeAsistencia/Registro.cs
+++ RegistroDeAsistencia/Registro.cs
@@ -1,10 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace RegistroDeAsistencia
 {
-    public partial class Form1 : Form
+    public partial class Registro : Form
     {
-        public Form1()
+        public Registro()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+
         }
     }
 }
