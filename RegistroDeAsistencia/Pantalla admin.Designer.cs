d55d652473ce8d25d7ced2645a763d27d08dec0d
abrahamrl13 <abrahamlaredo73@gmail.com>
2023-12-07 00:10:09 -0600

    ajustes a la pantalla de reportes
    Cambie las referencias de los elementos

---
+++ RegistroDeAsistencia/Pantalla admin.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace RegistroDeAsistencia
+{
+    partial class Pantalla_admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            textBox6 = new TextBox();
+            Column2 = new DataGridViewCheckBoxColumn();
+            Semestre = new DataGridViewTextBoxColumn();
+            Grupo = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Profesor = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Semestre";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Profesor";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Materia";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(108, 106);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(407, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(108, 63);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(108, 146);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(407, 23);
+            comboBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(660, 185);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Registrar grupo\r\n";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(624, 224);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Semestre";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(624, 352);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Profesor";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(624, 307);
+            label7.Name = "label7";
+            label7.Size = new Size(47, 15);
+            label7.TabIndex = 10;
+            label7.Text = "Materia";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(624, 262);
+            label8.Name = "label8";
+            label8.Size = new Size(40, 15);
+            label8.TabIndex = 11;
+            label8.Text = "Grupo";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(704, 216);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 12;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(704, 254);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(704, 299);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(704, 344);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 15;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(771, 391);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 23);
+            button1.TabIndex = 16;
+            button1.Text = "Agregar Grupo";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column2, Semestre, Grupo, Column1, Profesor });
+            dataGridView1.Location = new Point(70, 224);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(548, 214);
+            dataGridView1.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(70, 189);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(445, 23);
+            textBox6.TabIndex = 18;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "X";
+            Column2.Name = "Column2";
+            // 
+            // Semestre
+            // 
+            Semestre.HeaderText = "Semestre";
+            Semestre.Name = "Semestre";
+            // 
+            // Grupo
+            // 
+            Grupo.HeaderText = "Grupo";
+            Grupo.Name = "Grupo";
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Materia";
+            Column1.Name = "Column1";
+            // 
+            // Profesor
+            // 
+            Profesor.HeaderText = "Profesor";
+            Profesor.Name = "Profesor";
+            // 
+            // Pantalla_admin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(922, 450);
+            Controls.Add(textBox6);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Pantalla_admin";
+            Text = "Pantalla_admin";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TextBox textBox6;
+        private DataGridViewCheckBoxColumn Column2;
+        private DataGridViewTextBoxColumn Semestre;
+        private DataGridViewTextBoxColumn Grupo;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Profesor;
+    }
+}
---
+++ RegistroDeAsistencia/Pantalla_reportes.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace RegistroDeAsistencia
+{
+    partial class Pantalla_reportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MateriacomboBox2 = new ComboBox();
+            GrupocomboBox1 = new ComboBox();
+            ProfesortextBox1 = new TextBox();
+            Materialabel3 = new Label();
+            Profesorlabel2 = new Label();
+            Grupolabel1 = new Label();
+            Semestrelabel4 = new Label();
+            Fechainferiorlabel5 = new Label();
+            Fechasuperiorlabel6 = new Label();
+            SemestrecomboBox3 = new ComboBox();
+            busquedatextBox6 = new TextBox();
+            dataGridView1 = new DataGridView();
+            Column2 = new DataGridViewCheckBoxColumn();
+            Folio = new DataGridViewTextBoxColumn();
+            Fecha = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Profesor = new DataGridViewTextBoxColumn();
+            backupbutton1 = new Button();
+            FechainferiordateTimePicker1 = new DateTimePicker();
+            FechasuperiordateTimePicker2 = new DateTimePicker();
+            FormatodereportecheckBox1 = new CheckBox();
+            excelxmlcheckBox2 = new CheckBox();
+            excelxlsxcheckBox3 = new CheckBox();
+            exportarbutton2 = new Button();
+            exportarenbutton3 = new Button();
+            label7 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // MateriacomboBox2
+            // 
+            MateriacomboBox2.FormattingEnabled = true;
+            MateriacomboBox2.Location = new Point(105, 148);
+            MateriacomboBox2.Name = "MateriacomboBox2";
+            MateriacomboBox2.Size = new Size(407, 23);
+            MateriacomboBox2.TabIndex = 11;
+            // 
+            // GrupocomboBox1
+            // 
+            GrupocomboBox1.FormattingEnabled = true;
+            GrupocomboBox1.Location = new Point(105, 65);
+            GrupocomboBox1.Name = "GrupocomboBox1";
+            GrupocomboBox1.Size = new Size(121, 23);
+            GrupocomboBox1.TabIndex = 10;
+            // 
+            // ProfesortextBox1
+            // 
+            ProfesortextBox1.Location = new Point(105, 108);
+            ProfesortextBox1.Name = "ProfesortextBox1";
+            ProfesortextBox1.Size = new Size(407, 23);
+            ProfesortextBox1.TabIndex = 9;
+            // 
+            // Materialabel3
+            // 
+            Materialabel3.AutoSize = true;
+            Materialabel3.Location = new Point(20, 151);
+            Materialabel3.Name = "Materialabel3";
+            Materialabel3.Size = new Size(47, 15);
+            Materialabel3.TabIndex = 8;
+            Materialabel3.Text = "Materia";
+            // 
+            // Profesorlabel2
+            // 
+            Profesorlabel2.AutoSize = true;
+            Profesorlabel2.Location = new Point(20, 111);
+            Profesorlabel2.Name = "Profesorlabel2";
+            Profesorlabel2.Size = new Size(51, 15);
+            Profesorlabel2.TabIndex = 7;
+            Profesorlabel2.Text = "Profesor";
+            // 
+            // Grupolabel1
+            // 
+            Grupolabel1.AutoSize = true;
+            Grupolabel1.Location = new Point(27, 73);
+            Grupolabel1.Name = "Grupolabel1";
+            Grupolabel1.Size = new Size(40, 15);
+            Grupolabel1.TabIndex = 6;
+            Grupolabel1.Text = "Grupo";
+            Grupolabel1.Click += label1_Click;
+            // 
+            // Semestrelabel4
+            // 
+            Semestrelabel4.AutoSize = true;
+            Semestrelabel4.Location = new Point(605, 75);
+            Semestrelabel4.Name = "Semestrelabel4";
+            Semestrelabel4.Size = new Size(55, 15);
+            Semestrelabel4.TabIndex = 12;
+            Semestrelabel4.Text = "Semestre";
+            // 
+            // Fechainferiorlabel5
+            // 
+            Fechainferiorlabel5.AutoSize = true;
+            Fechainferiorlabel5.Location = new Point(605, 116);
+            Fechainferiorlabel5.Name = "Fechainferiorlabel5";
+            Fechainferiorlabel5.Size = new Size(79, 15);
+            Fechainferiorlabel5.TabIndex = 13;
+            Fechainferiorlabel5.Text = "Fecha Inferior";
+            // 
+            // Fechasuperiorlabel6
+            // 
+            Fechasuperiorlabel6.AutoSize = true;
+            Fechasuperiorlabel6.Location = new Point(605, 156);
+            Fechasuperiorlabel6.Name = "Fechasuperiorlabel6";
+            Fechasuperiorlabel6.Size = new Size(85, 15);
+            Fechasuperiorlabel6.TabIndex = 14;
+            Fechasuperiorlabel6.Text = "Fecha Superior";
+            // 
+            // SemestrecomboBox3
+            // 
+            SemestrecomboBox3.FormattingEnabled = true;
+            SemestrecomboBox3.Location = new Point(694, 67);
+            SemestrecomboBox3.Name = "SemestrecomboBox3";
+            SemestrecomboBox3.Size = new Size(121, 23);
+            SemestrecomboBox3.TabIndex = 15;
+            // 
+            // busquedatextBox6
+            // 
+            busquedatextBox6.Location = new Point(105, 189);
+            busquedatextBox6.Name = "busquedatextBox6";
+            busquedatextBox6.Size = new Size(445, 23);
+            busquedatextBox6.TabIndex = 20;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column2, Folio, Fecha, Column1, Profesor });
+            dataGridView1.Location = new Point(105, 224);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(546, 214);
+            dataGridView1.TabIndex = 19;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "X";
+            Column2.Name = "Column2";
+            // 
+            // Folio
+            // 
+            Folio.HeaderText = "Folio";
+            Folio.Name = "Folio";
+            // 
+            // Fecha
+            // 
+            Fecha.HeaderText = "Fecha";
+            Fecha.Name = "Fecha";
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Materia";
+            Column1.Name = "Column1";
+            // 
+            // Profesor
+            // 
+            Profesor.HeaderText = "Profesor";
+            Profesor.Name = "Profesor";
+            // 
+            // backupbutton1
+            // 
+            backupbutton1.Location = new Point(105, 473);
+            backupbutton1.Name = "backupbutton1";
+            backupbutton1.Size = new Size(238, 23);
+            backupbutton1.TabIndex = 21;
+            backupbutton1.Text = "HACER BACKUP EN LA BASE DE DATOS";
+            backupbutton1.UseVisualStyleBackColor = true;
+            // 
+            // FechainferiordateTimePicker1
+            // 
+            FechainferiordateTimePicker1.Location = new Point(694, 110);
+            FechainferiordateTimePicker1.Name = "FechainferiordateTimePicker1";
+            FechainferiordateTimePicker1.Size = new Size(200, 23);
+            FechainferiordateTimePicker1.TabIndex = 22;
+            // 
+            // FechasuperiordateTimePicker2
+            // 
+            FechasuperiordateTimePicker2.Location = new Point(694, 148);
+            FechasuperiordateTimePicker2.Name = "FechasuperiordateTimePicker2";
+            FechasuperiordateTimePicker2.Size = new Size(200, 23);
+            FechasuperiordateTimePicker2.TabIndex = 23;
+            FechasuperiordateTimePicker2.ValueChanged += dateTimePicker2_ValueChanged;
+            // 
+            // FormatodereportecheckBox1
+            // 
+            FormatodereportecheckBox1.AutoSize = true;
+            FormatodereportecheckBox1.Location = new Point(718, 267);
+            FormatodereportecheckBox1.Name = "FormatodereportecheckBox1";
+            FormatodereportecheckBox1.Size = new Size(173, 19);
+            FormatodereportecheckBox1.TabIndex = 25;
+            FormatodereportecheckBox1.Text = "Formato de Reporte(s)(PDF)";
+            FormatodereportecheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // excelxmlcheckBox2
+            // 
+            excelxmlcheckBox2.AutoSize = true;
+            excelxmlcheckBox2.Location = new Point(718, 292);
+            excelxmlcheckBox2.Name = "excelxmlcheckBox2";
+            excelxmlcheckBox2.Size = new Size(148, 19);
+            excelxmlcheckBox2.TabIndex = 26;
+            excelxmlcheckBox2.Text = "Archivo de Excel (XML)";
+            excelxmlcheckBox2.UseVisualStyleBackColor = true;
+            // 
+            // excelxlsxcheckBox3
+            // 
+            excelxlsxcheckBox3.AutoSize = true;
+            excelxlsxcheckBox3.Location = new Point(718, 317);
+            excelxlsxcheckBox3.Name = "excelxlsxcheckBox3";
+            excelxlsxcheckBox3.Size = new Size(150, 19);
+            excelxlsxcheckBox3.TabIndex = 27;
+            excelxlsxcheckBox3.Text = "Archivo de Excel (XLSX)";
+            excelxlsxcheckBox3.UseVisualStyleBackColor = true;
+            // 
+            // exportarbutton2
+            // 
+            exportarbutton2.Location = new Point(718, 362);
+            exportarbutton2.Name = "exportarbutton2";
+            exportarbutton2.Size = new Size(173, 29);
+            exportarbutton2.TabIndex = 28;
+            exportarbutton2.Text = "Exportar";
+            exportarbutton2.UseVisualStyleBackColor = true;
+            // 
+            // exportarenbutton3
+            // 
+            exportarenbutton3.Location = new Point(718, 409);
+            exportarenbutton3.Name = "exportarenbutton3";
+            exportarenbutton3.Size = new Size(173, 29);
+            exportarenbutton3.TabIndex = 29;
+            exportarenbutton3.Text = "Exportar en";
+            exportarenbutton3.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(716, 235);
+            label7.Name = "label7";
+            label7.Size = new Size(67, 15);
+            label7.TabIndex = 30;
+            label7.Text = "Exportar en";
+            // 
+            // Pantalla_reportes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(924, 524);
+            Controls.Add(label7);
+            Controls.Add(exportarenbutton3);
+            Controls.Add(exportarbutton2);
+            Controls.Add(excelxlsxcheckBox3);
+            Controls.Add(excelxmlcheckBox2);
+            Controls.Add(FormatodereportecheckBox1);
+            Controls.Add(FechasuperiordateTimePicker2);
+            Controls.Add(FechainferiordateTimePicker1);
+            Controls.Add(backupbutton1);
+            Controls.Add(busquedatextBox6);
+            Controls.Add(dataGridView1);
+            Controls.Add(SemestrecomboBox3);
+            Controls.Add(Fechasuperiorlabel6);
+            Controls.Add(Fechainferiorlabel5);
+            Controls.Add(Semestrelabel4);
+            Controls.Add(MateriacomboBox2);
+            Controls.Add(GrupocomboBox1);
+            Controls.Add(ProfesortextBox1);
+            Controls.Add(Materialabel3);
+            Controls.Add(Profesorlabel2);
+            Controls.Add(Grupolabel1);
+            Name = "Pantalla_reportes";
+            Text = "Pantalla_reportes";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox MateriacomboBox2;
+        private ComboBox GrupocomboBox1;
+        private TextBox ProfesortextBox1;
+        private Label Materialabel3;
+        private Label Profesorlabel2;
+        private Label Grupolabel1;
+        private Label Semestrelabel4;
+        private Label Fechainferiorlabel5;
+        private Label Fechasuperiorlabel6;
+        private ComboBox SemestrecomboBox3;
+        private TextBox busquedatextBox6;
+        private DataGridView dataGridView1;
+        private Button backupbutton1;
+        private DataGridViewCheckBoxColumn Column2;
+        private DataGridViewTextBoxColumn Folio;
+        private DataGridViewTextBoxColumn Fecha;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Profesor;
+        private DateTimePicker FechainferiordateTimePicker1;
+        private DateTimePicker FechasuperiordateTimePicker2;
+        private CheckBox FormatodereportecheckBox1;
+        private CheckBox excelxmlcheckBox2;
+        private CheckBox excelxlsxcheckBox3;
+        private Button exportarbutton2;
+        private Button exportarenbutton3;
+        private Label label7;
+    }
+}
