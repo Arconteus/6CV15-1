8246dbc9fec751f43a8d1f12686c51368a0f70e0
Arconteus <Arconteus@gmail.com>
2023-12-07 01:11:36 -0600

    Cambios de referencias
    +Se agrego el archivo app.config
    *Se cambiaron nombres a archivos y su ubicación para un mejor orden.

--- RegistroDeAsistencia/DataBaseHelper.cs
+++ RegistroDeAsistencia/DataBaseHelper.cs
@@ -1,15 +1,17 @@
 ﻿using System.Data.SQLite;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Configuration;
 
 public class DataBaseHelper
 {
     //==============================================
     // Variables de control de la base de datos
     //==============================================
-    private static string connectionString = @"Data Source=..\..\..\Files\LibraryManagementSystem.db;Version=3;";
-    private static string rootDataBase = @"..\..\..\Files\LibraryManagementSystem.db";
-    private static string structureQueryPath = @"..\..\..\Files\structureQuery.txt";
+    private static string connectionString = ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString;
+    private static string rootDataBase = ConfigurationManager.ConnectionStrings["rootDataBase"].ConnectionString;
+    private static string structureScriptPath = ConfigurationManager.ConnectionStrings["structureScriptPath"].ConnectionString;
     /**
      * Esta funcion inicializa la base de datos si es que no existe.
      **/
@@ -23,9 +25,24 @@ public class DataBaseHelper
                 connection.Open();
                 using (var command = new SQLiteCommand(connection) )
                 {
-                    command.CommandText = File.ReadAllText(structureQueryPath);
+                    command.CommandText = File.ReadAllText(structureScriptPath);
                     command.ExecuteNonQuery();
                 }
+            }
+        }
+    }
+    public static void addCodigoGrupo(string codigoGrupoToAdd) 
+    {
+        using(var connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            using(SQLiteCommand command = new SQLiteCommand(connection) )
+            {
+                command.CommandText=
+                    @"INSERT INTO ctl_codigoGrupo (desc_grupo) values (@codigo_grupo)";
+                command.Parameters.AddWithValue("@codigo_grupo",codigoGrupoToAdd);
+                command.ExecuteNonQuery();
+                command.Parameters.Clear();
             }
         }
     }
--- RegistroDeAsistencia/Test.cs
+++ RegistroDeAsistencia/Test.cs
@@ -6,5 +6,14 @@ namespace RegistroDeAsistencia
         {
             InitializeComponent();
         }
+
+        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
