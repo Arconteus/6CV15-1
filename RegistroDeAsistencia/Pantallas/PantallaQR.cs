aabddbdac1c2f23bf5734c63427a6416838c7a92
Laymax4 <max_vazquez-mats@hotmail.com>
2024-01-09 23:43:17 -0600

    QR FUNCIONAL

--- RegistroDeAsistencia/Pantallas/PantallaQR.cs
+++ RegistroDeAsistencia/Pantallas/PantallaQR.cs
@@ -1,5 +1,6 @@
 ﻿using AForge.Video;
 using AForge.Video.DirectShow;
+using ZXing;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,19 +11,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using ZXing.QrCode;
+using ZXing.Windows.Compatibility;
 
 namespace RegistroDeAsistencia
 {
     public partial class PantallaQR : Form
     {
-        //==============================================================================================
         [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
         private extern static void ReleaseCapture();
         [DllImport("user32.dll", EntryPoint = "SendMessage")]
         private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int sParam, int lParam);
-        //==============================================================================================
+
         FilterInfoCollection _filterInfoCollection;
         VideoCaptureDevice _videoCaptureDevice;
+        private Bitmap capturedImage;
 
         public PantallaQR()
         {
@@ -30,13 +33,20 @@ namespace RegistroDeAsistencia
 
             _filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
 
-            foreach (FilterInfo filterInfo in _filterInfoCollection)
-                cmbCameras.Items.Add(filterInfo.Name);
+            if (_filterInfoCollection.Count == 0)
+            {
+                cmbCameras.Items.Add("No se ha encontrado ninguna cámara en el sistema");
+                cmbCameras.Enabled = false;
+            }
+            else
+            {
+                foreach (FilterInfo filterInfo in _filterInfoCollection)
+                    cmbCameras.Items.Add(filterInfo.Name);
 
-            cmbCameras.SelectedIndex = 0;
-            _videoCaptureDevice = new VideoCaptureDevice();
+                cmbCameras.SelectedIndex = 0;
+                _videoCaptureDevice = new VideoCaptureDevice();
+            }
 
-            // Vincula el evento SelectedIndexChanged al método cmbCameras_SelectedIndexChanged
             cmbCameras.SelectedIndexChanged += cmbCameras_SelectedIndexChanged;
         }
 
@@ -54,21 +64,17 @@ namespace RegistroDeAsistencia
             }
         }
 
-        private void ScanButton_Click(object sender, EventArgs e)
-        {
-            _videoCaptureDevice.NewFrame += _videoCaptureDevice_NewFrame;
-            _videoCaptureDevice.Start();
-        }
-
         private void _videoCaptureDevice_NewFrame(object sender, NewFrameEventArgs e)
         {
             pictCamImagem.Image = (System.Drawing.Image)e.Frame.Clone();
+            capturedImage = (Bitmap)e.Frame.Clone();
         }
 
         private void CloseButtonQR_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
         private void Header_MouseDown(object sender, MouseEventArgs e)
         {
             ReleaseCapture();
@@ -83,6 +89,50 @@ namespace RegistroDeAsistencia
         private void LectorQRButton_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void CamPB_Click(object sender, EventArgs e)
+        {
+            if (_videoCaptureDevice != null && _videoCaptureDevice.IsRunning)
+            {
+                MessageBox.Show("La cámara ya está activada.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            int selectedIndex = cmbCameras.SelectedIndex;
+
+            if (selectedIndex >= 0 && selectedIndex < _filterInfoCollection.Count)
+            {
+                _videoCaptureDevice = new VideoCaptureDevice(_filterInfoCollection[selectedIndex].MonikerString);
+                _videoCaptureDevice.NewFrame += _videoCaptureDevice_NewFrame;
+                _videoCaptureDevice.Start();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona una cámara válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Scan_Click(object sender, EventArgs e)
+        {
+            if (capturedImage != null)
+            {
+                var barcodeReader = new BarcodeReader();
+                var result = barcodeReader.Decode(capturedImage);
+
+                if (result != null)
+                {
+                    MessageBox.Show($"Código QR capturado: {result.Text}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No se ha capturado un código QR legible.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No hay ninguna imagen capturada.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
